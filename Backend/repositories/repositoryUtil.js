1aecb2b5318025d0cc165956ade8bfdc9b2a4445
Ray <zruiwh@cn.ibm.com>
2017-05-29 21:24:15 +0800

    bug fix

--- Backend/repositories/repositoryUtil.js
+++ Backend/repositories/repositoryUtil.js
@@ -1,42 +1,5 @@
 var CamproError = require('../models/CamproError');
 
-exports.buildSearchRegExp = function(string){
-	var alphabet = exports.alphabetize(string,{
-		separator:'|'
-	});
-
-	var exp = "";
-
-	if (alphabet.indexOf('|') == -1) {
-		for(var i in alphabet){
-			exp = exp + alphabet[i] + "[A-Za-z0-9\\|]*";
-		}
-	}else{
-		exp = alphabet.replace('|','\\|');
-	}
-
-	return RegExp(exp,'i');
-};
-
-exports.alphabetize = function(string,options){
-	var separator = "";
-	if (!!options && 'separator' in options) {
-		separator = options.separator;
-	}
-
-	var pinyinArray = pinyin(string,{
-		style:pinyin.STYLE_NORMAL
-	});
-
-	var pinyinString = "";
-	for(var i in pinyinArray){
-		if (i > 0) pinyinString = pinyinString + separator;
-	  	pinyinString = pinyinString + pinyinArray[i];
-	}
-
-	return pinyinString;
-};
-
 exports.paging = function(model,conditions,options,population) {
 	var totalCount = null;
 
